d10835819b35f4873bda1cbc2bcd91e575e537fc
Avshalom <avshalom.tam@gmail.com>
2020-12-29 21:21:21 +0200

    connected to mongoDB established

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // BASE SETUP
 // =============================================================================
 
+require('./db/mongoose');
+
 // call the packages we need
 var express = require('express');           // call express
 var app = express();                        // define our app using express
@@ -14,11 +16,14 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 3000;        // set our port
 
+// var mongoose   = require('mongoose');
+// mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// test route to make sure everything is working (accessed at GET http://localhost:3000/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
